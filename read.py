7b9b608ac1dc37fe8a84a7651546883efc7aae8f
Tina <tina19960712520@gmail.com>
2022-12-19 15:51:37 +0800

    add filters

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ with open ('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1 
-        if count % 1000000 == 0:
+        if count % 1000 == 0:
             print(len(data))
 print('檔案讀取完了,總共有', len(data), '筆資料')
 
@@ -19,4 +19,10 @@ for d in data:
         new.append(d)
 print ('一共有', len(new), '筆留言長度小於100')
 print(new[0])
-print(new[1])
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print ('一共有', len(good), '筆留言提到good')
+print(good[0])
